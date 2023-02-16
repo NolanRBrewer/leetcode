ba6db8ef980e11c27bfaa9cdcdfc5ab00d7fe1db
NolanRBrewer <nolan.r.brewer@gmail.com>
2023-02-16 15:25:42 -0500

    Finished pivot index leetcode

---
+++ solutions/running_sums.py
@@ -0,0 +1,16 @@
+nums = [2, 1, -1]
+
+
+def running_sum(nums: list[int]) -> list[int]:
+
+    left_sum, right_sum = 0, sum(nums)
+
+    for index, num in nums:
+        
+
+
+    
+    return -1 
+
+
+print(running_sum(nums))
